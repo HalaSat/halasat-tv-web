258ca76a5292c3097dbbd88b2c412c7862e421ad
HalaSat <halasat@HalaSats-MacBook-Pro.local>
2018-12-13 10:06:14 +0300

    added a level selector

--- js/index.js
+++ js/index.js
@@ -1,142 +1,62 @@
-
-// Data
-let channels = [{
-    title: 'beIN Sports 1HD',
-    url: './assets/bein.png',
-    accent: 'purple',
-}, {
-    title: 'beIN Sports 2HD',
-    url: './assets/bein.png',
-    accent: 'purple',
-}, {
-    title: 'beIN Sports 3HD',
-    url: './assets/bein.png',
-    accent: 'purple',
-}, {
-    title: 'beIN Sports 4HD',
-    url: './assets/bein.png',
-    accent: 'purple',
-}, {
-    title: 'beIN Sports 5HD',
-    url: './assets/bein.png',
-    accent: 'purple',
-}, {
-    title: 'beoutQ 1HD',
-    url: './assets/beoutq.png',
-    accent: 'green',
-}, {
-    title: 'beoutQ 2HD',
-    url: './assets/beoutq.png',
-    accent: 'green',
-}, {
-    title: 'OSN Ya Hala Al Oula',
-    url: './assets/osn-yahala.png',
-    accent: 'purple',
-}, {
-    title: 'OSN Ya Hala HD',
-    url: './assets/osn-yahala.png',
-    accent: 'purple',
-}, {
-    title: 'On Sport',
-    url: './assets/onsport.jpg',
-    accent: 'blue',
-}, {
-    title: 'Planet Earth HD',
-    url: './assets/planet-earth.jpeg',
-    accent: 'black',
-}, {
-    title: 'Iraqia News',
-    url: './assets/imn.jpg',
-    accent: 'blue',
-}, {
-    title: 'Discovery HD',
-    url: './assets/discovery.png',
-    accent: 'black',
-}, {
-    title: 'OSN Movies HD',
-    url: './assets/osn-movies.jpg',
-    accent: 'yellow',
-}, {
-    title: 'NAT GEO Wild HD',
-    url: './assets/natgeo-wild.png',
-    accent: 'yellow',
-}, {
-    title: 'Animal Planet HD',
-    url: './assets/animal-planet.png',
-    accent: 'yellow',
-}, {
-    title: 'Travel Channel',
-    url: './assets/travel-channel.png',
-    accent: 'yellow',
-}, {
-    title: 'Discovery Science HD',
-    url: './assets/discovery-science.png',
-    accent: 'yellow',
-}, {
-    title: 'H2 HD',
-    url: './assets/h2.png',
-    accent: 'yellow',
-}, {
-    title: 'FIX & FOXI',
-    url: './assets/fixandfoxi.png',
-    accent: 'yellow',
-}, {
-    title: 'ITV Choice',
-    url: './assets/itv.png',
-    accent: 'black',
-}, {
-    title: 'Stereo Music',
-    url: './assets/stereomusic.png',
-    accent: 'green',
-}, {
-    title: 'NAT GEO Wild HD',
-    url: './assets/natgeowild.png',
-    accent: 'yellow',
-}, {
-    title: 'NAT GEO Wild HD',
-    url: './assets/natgeowild.png',
-    accent: 'yellow',
-}, {
-    title: 'NAT GEO Wild HD',
-    url: './assets/natgeowild.png',
-    accent: 'yellow',
-}, {
-    title: 'NAT GEO Wild HD',
-    url: './assets/natgeowild.png',
-    accent: 'yellow',
-}, {
-    title: 'NAT GEO Wild HD',
-    url: './assets/natgeowild.png',
-    accent: 'yellow',
-}, {
-    title: 'NAT GEO Wild HD',
-    url: './assets/natgeowild.png',
-    accent: 'yellow',
-}, {
-    title: 'NAT GEO Wild HD',
-    url: './assets/natgeowild.png',
-    accent: 'yellow',
-}, {
-    title: 'NAT GEO Wild HD',
-    url: './assets/natgeowild.png',
-    accent: 'yellow',
-}, {
-    title: 'NAT GEO Wild HD',
-    url: './assets/natgeowild.png',
-    accent: 'yellow',
-}, {
-    title: 'NAT GEO Wild HD',
-    url: './assets/natgeowild.png',
-    accent: 'yellow',
-},];
+const fallbackColor = "#1382ba";
+const fallbackImage = "./assets/channel-logo.png";
 
 window.onload = () => {
-    let items = channels.map(item => `
-        <div class="channel ${item.accent}">
-            <img alt="" src="${item.url}"/>
-            <p>${item.title}</p>
-        </div>
-    `);
-    let channelsList = document.getElementById('channels-list');
-    items.forEach(item => channelsList.innerHTML += item);
+  const player = new Clappr.Player({
+    source: "http://192.168.37.2:1935/bein1/1_adaptive.m3u8",
+    mute: false,
+    autoPlay: true,
+    parentId: "#video",
+    width: "100%",
+    height: "100%",
+    autoPlay: true,
+    plugins: [LevelSelector],
+    levelSelectorConfig: {
+      title: "Quality",
+      labels: {
+        2: "Very High ", // 500kbps
+        1: "High ", // 240kbps
+        0: "Med " // 120kbps
+      },
+      labelCallback: function(playbackLevel, customLabel) {
+        return customLabel + playbackLevel.level.height + "p"; // High 720p
+      }
+    }
+  });
+  const changeStream = channel => {
+    player.configure({
+      source: `http://192.168.37.2:1935/${channel.app}/${
+        channel.streamname
+      }_adaptive.m3u8`
+    });
+  };
+  fetch("http://91.106.32.84/api.php")
+    .then(r => r.json())
+    .then(channels => {
+      let items = channels.map(
+        item => `
+              <div class="channel">
+                  <div class="before" style="background-color: ${item.color ||
+                    fallbackColor};">
+                  <p>Watch ${item.title}</p>
+                  </div>
+                  <img alt="" src="${
+                    item.image
+                      ? "http://91.106.32.84/" + item.image
+                      : fallbackImage
+                  }" />
+                  <p>${item.title}</p>
+              </div>
+          `
+      );
+      const channelsList = document.getElementById("channels-list");
+      items.forEach(item => (channelsList.innerHTML += item));
+      for (i in channelsList.children) {
+        const channel = channelsList.children[i];
+        const before = channel.getElementsByClassName("before")[0];
+        const item = channels[i];
+        channel.addEventListener("click", () => changeStream(item));
+        before.addEventListener("click", () => changeStream(item));
+      }
+    });
 };
